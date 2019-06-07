908600cde4d1d7c27c182c8a20a6a5b4cd59c2bd
Jelle Brouwer <j.brouwer@pepperbase.nl>
2019-06-07 16:44:47 +0200

    Fix build script

--- config/webpack-lib.config.js
+++ config/webpack-lib.config.js
@@ -1,30 +1,25 @@
-const path = require('path');
-const webpack = require('webpack');
 const WebpackConfig = require('webpack-config').Config;
 const package = require('../package.json');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = new WebpackConfig().extend('./config/webpack-base.config.js').merge({
-   mode: 'production',
-    entry: {
-        app: [
-            "./src/components/milSymbols.module.ts",
-            "./src/sass/main.scss"
-        ]
-    },
-    output: {
-        library: package.name,
-        libraryTarget: "umd",
-        umdNamedDefine: true
-    },
-    externals: {
-        "angular": "angular",
-        "milsymbol": "milsymbol"
-    },
-    plugins: [
-        new CleanWebpackPlugin(['build'], {
-            root: path.resolve(__dirname, '../'),
-            verbose: true
-        })
+  mode: 'production',
+  entry: {
+    app: [
+      "./src/components/milSymbols.module.ts",
+      "./src/sass/main.scss"
     ]
+  },
+  output: {
+    library: package.name,
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  externals: {
+    "angular": "angular",
+    "milsymbol": "milsymbol"
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 });
